169bf316b863961fe3f39edafe328801b14758a1
kg1992 <kyunggeun1992@gmail.com>
2022-11-05 16:59:45 +0900

    test-cors request to a localhost and a google cloud app engien hosted nodejs server

--- index.js
+++ index.js
@@ -1,5 +1,18 @@
+const remoteUrl = "https://quiz-app-367510.uc.r.appspot.com/quote";
+const localUrl = "http://localhost:8080/quote";
+
+console.log("send request to localhost nodejs server")
 $.ajax({
-    url: "https://quiz-app-367510.uc.r.appspot.com/quiz",
+    url: localUrl,
+    type: 'GET'
+}).then(function(r){
+    console.log(r);
+    document.body.appendChild(r);
+});
+
+console.log("send request to remote google cloud app engine hosted nodejs server")
+$.ajax({
+    url: remoteUrl,
     type: 'GET'
 }).then(function(r){
     console.log(r);
