ec43538bfdc4727e1fb28bdf339eac348f96dc98
kg1992 <kyunggeun1992@gmail.com>
2022-11-09 23:36:20 +0900

    add favicon / add quiz-module example

--- index.js
+++ index.js
@@ -1,20 +1,6 @@
-const remoteUrl = "https://quiz-app-367510.uc.r.appspot.com/quote";
-const localUrl = "http://localhost:8080/quote";
-
-console.log("send request to localhost nodejs server")
-$.ajax({
-    url: localUrl,
-    type: 'GET'
-}).then(function(r){
-    console.log(r);
-    document.body.appendChild(r);
-});
-
-console.log("send request to remote google cloud app engine hosted nodejs server")
-$.ajax({
-    url: remoteUrl,
-    type: 'GET'
-}).then(function(r){
-    console.log(r);
-    document.body.appendChild(r);
+window.addEventListener('load', function(event){
+    var options = {
+        // url : "http://localhost:8080"
+    }
+    quizModule.start(options);
 });
