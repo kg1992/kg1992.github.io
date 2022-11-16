3d67f4d4538e5b0320ddb1f68f6b907707038ef3
kg1992 <kyunggeun1992@gmail.com>
2022-11-16 20:24:47 +0900

    change link : localhost → google cloud

--- index.js
+++ index.js
@@ -1,7 +1,4 @@
 window.addEventListener('load', function(event){
-    var options = {
-        url : "http://localhost:8080"
-    }
-    quizModule.start(options);
+    quizModule.start();
     hljs.highlightAll();
 });
